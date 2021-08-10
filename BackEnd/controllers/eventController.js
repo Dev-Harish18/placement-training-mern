c5a6fc5ee4d4d9a5bb7d4f11debc5eb651c973ef
Harish V <harishvilar91@gmail.com>
2021-08-10 23:32:31 +0530

    Update eventController.js

--- BackEnd/controllers/eventController.js
+++ BackEnd/controllers/eventController.js
@@ -3,15 +3,19 @@ const User = require("../models/User");
 const AppError = require("../utils/AppError");
 const catchAsync = require("../utils/catchAsync");
 
+//Get all events (Quizzes)
 exports.allEvents = catchAsync(async (req, res, next) => {
+  //Fetch quizes and sort based on createdAt field
   let events = await Event.find().sort("-createdAt");
+  //Constructing meaningful result set
   events = events.map((event) => {
     return {
-      ...event.toObject(),
-      attended: event.users.includes(req.user._id),
-      users: undefined,
+      ...event.toObject(),//existing properties
+      attended: event.users.includes(req.user._id), //If user attended the current quiz
+      users: undefined, 
     };
   });
+ //Sending the events array as JSON
   res.status(200).json({
     status: "success",
     total: events.length,
@@ -22,19 +26,24 @@ exports.allEvents = catchAsync(async (req, res, next) => {
 });
 
 exports.validateId = (req, res, next) => {
+  //check for valid MongoID
   if (req.params.id.length !== 24)
     return next(new AppError("404", "Invalid event id"));
   next();
 };
 
 exports.getEvent = catchAsync(async (req, res, next) => {
+  //Get ID from url
   let event = await Event.findById(req.params.id);
+  //Invalid quiz IDs
   if (!event) return next(new AppError("404", "No such events found"));
+  //Constructing result set
   event = {
     ...event.toObject(),
     attended: event.users.includes(req.user._id),
     users: undefined,
   };
+  //Sending current event
   res.status(200).json({
     status: "success",
     data: {
@@ -44,18 +53,21 @@ exports.getEvent = catchAsync(async (req, res, next) => {
 });
 
 exports.handleSubmit = catchAsync(async (req, res, next) => {
+  //Update the attended event 
   const event = await Event.findByIdAndUpdate(
     req.params.id,
     {
-      $push: { users: req.user._id },
+      $push: { users: req.user._id },//Add submitted user to attended users list
     },
-    { new: true, upsert: true }
+    { new: true, upsert: true }//upsert --> if empty create one
   );
-
+  //If invalid ID
+  if (!event) return next(new AppError(404, "No such Events found"));
+  //Update the user with current event
   const user = await User.findByIdAndUpdate(
     req.user._id,
     {
-      $push: {
+      $push: {//add current quiz to attended quizes list
         events: {
           eventId: req.params.id,
           time: req.body.time,
@@ -63,10 +75,9 @@ exports.handleSubmit = catchAsync(async (req, res, next) => {
         },
       },
     },
-    { new: true, upsert: true }
+    { new: true, upsert: true }//upsert --> if empty create one
   );
 
-  if (!event) return next(new AppError(404, "No such Events found"));
   res.status(201).json({
     status: "success",
     data: {
@@ -77,6 +88,7 @@ exports.handleSubmit = catchAsync(async (req, res, next) => {
 });
 
 exports.addEvent = catchAsync(async (req, res, next) => {
+  //Creating event 
   const event = await Event.create(req.body);
   res.status(201).json({
     status: "success",
@@ -86,6 +98,7 @@ exports.addEvent = catchAsync(async (req, res, next) => {
   });
 });
 exports.editEvent = catchAsync(async (req, res, next) => {
+  //Edit current event based on id in url
   const event = await Event.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
@@ -99,7 +112,8 @@ exports.editEvent = catchAsync(async (req, res, next) => {
   });
 });
 exports.deleteEvent = catchAsync(async (req, res, next) => {
-  await Event.findByIdAndDelete(req.params.id);
+  
+  await Event.findByIdAndDelete(req.params.id);//Deleting event
   const user = await User.findById(req.user._id);
 
   user.events = user.events.filter((evt) => evt.eventId != req.params.id);
@@ -110,7 +124,9 @@ exports.deleteEvent = catchAsync(async (req, res, next) => {
     .status(200)
     .json({ status: "success", message: "Quiz deleted successfully" });
 });
+
 exports.getStats = catchAsync(async (req, res, next) => {
+  //Populating with user data
   let event = await Event.findById(req.params.id).populate("users");
   if (!event) return next(new AppError(404, "No such Events found"));
   //Filter only current events stuff
